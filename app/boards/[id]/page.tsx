979f2ab188bdff4a685c945c9d69b54d23fea017
tylercurtispohl <tylercurtispohl@gmail.com>
2024-04-03 16:55:05 -0700

    resolve type error

--- app/boards/[id]/page.tsx
+++ app/boards/[id]/page.tsx
@@ -1,14 +1,15 @@
 import { BoardT } from "@/app/types";
 import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { redirect } from "next/navigation";
 
 const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-export default withPageAuthRequired(async function Page({
-  params,
-}: {
-  params: { id: number };
-}) {
-  const { id } = params;
+export default withPageAuthRequired(async function Page({ params }) {
+  const id = params?.id;
+
+  if (!id) {
+    redirect("/boards");
+  }
 
   const response = await fetch(`${baseUrl}/api/board/${id}`);
   const board = (await response.json()) as BoardT;
@@ -23,4 +24,4 @@ export default withPageAuthRequired(async function Page({
         ))}
     </div>
   );
-} as any);
+});
