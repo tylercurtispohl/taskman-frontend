60f3551b9f3810edc0bc00eef558afd0698fb8a7
tylercurtispohl <tylercurtispohl@gmail.com>
2024-04-04 14:12:00 -0700

    add bearer token to api call (doesn't work right now)

--- app/boards/page.tsx
+++ app/boards/page.tsx
@@ -5,14 +5,22 @@ import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
 const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export default withPageAuthRequired(async function Page() {
-  const response = await fetch(`${baseUrl}/api/board`);
-  const boards = (await response.json()) as BoardT[];
   const session = await getSession();
-  const user = session?.user;
+  // const user = session?.user;
   const accessToken = session?.accessToken;
 
-  console.log(user);
+  // console.log(user);
   console.log(accessToken);
+  const response = await fetch(`${baseUrl}/api/board`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  console.log(response);
+
+  const boards = (await response.json()) as BoardT[];
 
   return (
     <>
